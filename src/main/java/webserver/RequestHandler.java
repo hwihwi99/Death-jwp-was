982ac5ba569fb867f47cf69951d4b8df9baf4047
hwihwi99 <chamgrace@naver.com>
2023-12-30 02:20:31 +0900

    feat 3장 요구사항 완료 및 4장 코드 보완 완료~!

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,8 +2,10 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Map;
 
 import db.DataBase;
@@ -44,29 +46,29 @@ public class RequestHandler extends Thread {
                 return;
             }
 
-            String[] inputArr = str.split(" ");
+            String[] tokens = str.split(" ");
             int contentLength = 0;
-            String logined = "";
+            boolean logined = false;
 
             while (!str.isEmpty()) {
                 str = bf.readLine();
                 log.debug("header : {}", str);
 
-                if(str.contains("Content-Length")) {
-                    contentLength = Integer.parseInt(str.split(":")[1].trim());
+                if(str.contains("Content-Length")) { // 본문 데이터에 대한 길이가 여기에 담겨져서 내려온다.
+                    contentLength = getContentLength(str);
                 }
-                if(str.contains("Set-Cookie")) {
-                    logined = HttpRequestUtils.parseCookies(str.split(":")[1]).get("logined");
+                if(str.contains("Cookie")) {
+                    logined = isLogin(str);
                 }
             }
 
 
-            String status = inputArr[0]; // 요청상태
-            String requestUrl = inputArr[1]; // url 요청 형태
-            String etc = inputArr[2]; // HTTP/1:1 프로토콜 정보
+            String httpMethods = tokens[0]; // 요청상태
+            String requestUrl = tokens[1]; // url 요청 형태
+            String httpVersion = tokens[2]; // HTTP/1:1 프로토콜 정보
 
             // 요구사항 1
-            if (status.equals("GET") && requestUrl.equals("/index.html")) {
+            if (httpMethods.equals("GET") && requestUrl.equals("/index.html")) {
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] bytes = Files.readAllBytes(Paths.get("./webapp" + requestUrl));
                 response200Header(dos, bytes.length);
@@ -74,7 +76,7 @@ public class RequestHandler extends Thread {
             }
 
             // 요구사항 2 SUB - 회원가입 페이지로 이동
-            if(status.equals("GET") && requestUrl.equals("/user/form.html")) {
+            if(httpMethods.equals("GET") && requestUrl.equals("/user/form.html")) {
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] bytes = Files.readAllBytes(Paths.get("./webapp" + requestUrl));
                 response200Header(dos, bytes.length);
@@ -82,77 +84,89 @@ public class RequestHandler extends Thread {
             }
 
             // 요구사항 2
-            if (status.equals("GET") && requestUrl.contains("/user/create")) {
-
+            if (httpMethods.equals("GET") && requestUrl.contains("/user/create")) {
                 String data = requestUrl.split("\\?")[1];
-                String[] input = data.split("&");
-
-                String userId = HttpRequestUtils.parseQueryString(input[0]).get("userId");
-                String password = HttpRequestUtils.parseQueryString(input[1]).get("password");
-                String name = HttpRequestUtils.parseQueryString(input[2]).get("name");
-                String email = HttpRequestUtils.parseQueryString(input[3]).get("email");
-
-                User user = new User(userId, password, name, email);
-                DataBase.addUser(user);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(data);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                DataBase.addUser(user); // DB저장
+                log.debug("User : {}", user);
             }
 
-
             // 요구사항 3
-            if (status.equals("POST") && requestUrl.equals("/user/create")) {
+            if (httpMethods.equals("POST") && requestUrl.equals("/user/create")) {
                 String data = IOUtils.readData(bf, contentLength);
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                System.out.println(data);
-                String[] input = data.split("&");
-                String userId = HttpRequestUtils.parseQueryString(input[0]).get("userId");
-                String password = HttpRequestUtils.parseQueryString(input[1]).get("password");
-                String name = HttpRequestUtils.parseQueryString(input[2]).get("name");
-                String email = HttpRequestUtils.parseQueryString(input[3]).get("email");
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-                User user = new User(userId, password, name, email);
+                Map<String, String> params =  HttpRequestUtils.parseQueryString(data);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 DataBase.addUser(user);
+
+                log.debug("User : {}", user);
+
 
                 // 요구사항 4
                 DataOutputStream dos = new DataOutputStream(out);
-                byte[] bytes = Files.readAllBytes(Paths.get("./webapp/index.html"));
-                response302Header(dos, bytes.length);
-                responseBody(dos, bytes);
+                response302Header(dos, "/index.html");
             }
 
 
-            if (status.equals("GET") && requestUrl.equals("/user/login.html")) {
+            // 요구사항 5 로그인 화면 진입
+            if (httpMethods.equals("GET") && requestUrl.equals("/user/login.html")) {
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] bytes = Files.readAllBytes(Paths.get("./webapp/user/login.html"));
                 response200Header(dos, bytes.length);
                 responseBody(dos, bytes);
             }
 
+
             // 요구사항 5
-            if (status.equals("POST") && requestUrl.equals("/user/login")) {
+            if (httpMethods.equals("POST") && requestUrl.equals("/user/login")) {
                 String data = IOUtils.readData(bf, contentLength);
-                String[] input = data.split("&");
-                String userId = HttpRequestUtils.parseQueryString(input[0]).get("userId");
-                String password = HttpRequestUtils.parseQueryString(input[1]).get("password");
+                Map<String, String> params = HttpRequestUtils.parseQueryString(data);
+                User userInfo = DataBase.findUserById(params.get("userId"));
 
-                User userInfo = DataBase.findUserById(userId);
-                String loginStatus = "false";
-
-                if(userInfo !=null && userInfo.getPassword().equals(password)) {
-                    loginStatus = "true";
+                if (userInfo == null) {
+                    reaaponseResource(out, "/user/login_failed.html");
+                    return;
                 }
 
-                // logined if분기문 잘 처리하기
-                if(loginStatus.equals("true")) {
+                if(userInfo.getPassword().equals(params.get("password"))) {
                     DataOutputStream dos = new DataOutputStream(out);
-                    byte[] bytes = Files.readAllBytes(Paths.get("./webapp/index.html"));
-                    response302HeaderFound(dos, bytes.length, true);
-                    responseBody(dos, bytes);
-                }else if(loginStatus.equals("false")) {
-                    DataOutputStream dos = new DataOutputStream(out);
-                    byte[] bytes = Files.readAllBytes(Paths.get("./webapp/user/login_failed.html"));
-                    response302HeaderFound(dos, bytes.length, false);
-                    responseBody(dos, bytes);
+                    response302HeaderLoginSuccessHeader(dos);
+                }else {
+                    reaaponseResource(out, "/user/login_failed.html");
                 }
+            }
+
+            // 요구사항 6
+            if(httpMethods.equals("GET") && requestUrl.equals("/user/list")) {
+                if(!logined) {
+                    reaaponseResource(out, "/user/login.html");
+                    return;
+                }
+
+                Collection<User> userList = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border = '1'>");
+                for(User user : userList) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+
+                byte[] body = sb.toString().getBytes();
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
+            // 요구사항 7
+            if(requestUrl.endsWith(".css")) {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + requestUrl).toPath());
+                response200CssHeader(dos, body.length);
+                responseBody(dos, body);
             }
 
         } catch (IOException e) {
@@ -171,10 +185,10 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void response302Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent) {
         try {
-            dos.writeBytes("HTTP/1.1 302 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -182,12 +196,28 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void response302HeaderFound(DataOutputStream dos, int lengthOfBodyContent, boolean logined) {
+    private void response302Header(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + url + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void reaaponseResource(OutputStream out, String url) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private void response302HeaderLoginSuccessHeader(DataOutputStream dos) {
         try {
             dos.writeBytes("HTTP/1.1 302 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("Set-Cookie: logined="+logined+"\r\n");
+            dos.writeBytes("Set-Cookie: logined= true\r\n");
+            dos.writeBytes("Location: /index.html \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -201,5 +231,22 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private int getContentLength(String line) {
+        String[] headerTokens = line.split(":");
+        return Integer.parseInt(headerTokens[1].trim());
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null) {
+            // 쿠키가 없으면
+            return false;
+        }
+
+        return Boolean.parseBoolean(value);
     }
 }
